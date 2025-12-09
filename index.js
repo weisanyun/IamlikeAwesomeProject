863203c717eebd7d0cabc27e10fa816a648401e5
huangryeahwade <276856133@qq.com>
2025-12-09 21:55:12 +0800

    feat: 添加多个功能模块和组件
    - 新增图片模块相关功能，包括本地和网络图片加载
    - 添加样式表模块，包含flex布局和宽高比示例
    - 实现状态管理模块，包含商品列表和计数器功能
    - 新增文本输入组件，支持受控和非受控模式
    - 添加多种列表组件，包括ScrollView、FlatList和RecyclerListView
    - 引入基础工具类和配置文件

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
+import App from './src/08_List/ScrollViewExample.tsx';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
---
+++ src/04_State/App.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import ProductTable from './ProductTable';
+
+export default function App() {
+  return (
+    <SafeAreaView style={{marginHorizontal: 30}}>
+      <ProductTable />
+    </SafeAreaView>
+  );
+}
